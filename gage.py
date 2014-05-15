32a2c60e2f285e18a1cfa620f24b005384277c40
Alex Kerney <abk@mac.com>
2014-05-15 16:25:38 -0400

    bad ) and , positioning

--- gage.py
+++ gage.py
@@ -4,5 +4,5 @@ import ultrasound
 # import power
 
 print "DepthAIN is " + config.DepthAIN
-print "The depth in inches is " + str(round(ultrasound.checkDepth(config.DepthAIN,6)), 2)
+print "The depth in inches is " + str(round(ultrasound.checkDepth(config.DepthAIN,6), 2))
 print ""
