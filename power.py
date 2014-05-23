baddf0c3942f373ac6b5775bdac6199a68aec357
Alex Kerney <abk@mac.com>
2014-05-23 07:03:14 -0400

    Starting to work on power monitoring

--- power.py
+++ power.py
@@ -2,6 +2,24 @@
 # import Adafruit_BBIO.ADC as ADC
 import time
 from ina.Subfact_ina219 import INA219
+import numpy
 
-# ADC.setup()
+ina = INA219()
 
+def checkPower(j=3):
+	
+	currentPowerList = [] # create an empty array to store the power values
+
+	#for i in range(0,j):
+	#	ser.open()
+	#	ser.flushOutput()
+	#	ser.flushInput()
+	#	currentDepthList.append(int(ser.read(5)[1:4]))
+	#	ser.close()
+
+	#	time.sleep(.5) # Take a half second between reading ranges
+		
+	#currentDepth = numpy.mean(currentDepthList)
+	
+	currentPower.append(ina.getBusVoltage_V()
+	return currentPower
