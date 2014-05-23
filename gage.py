d605002f0575f3429648abef0c9f9002c4285935
Alex Kerney <abk@mac.com>
2014-05-22 20:54:12 -0400

    Lets try sending samples to the server

--- gage.py
+++ gage.py
@@ -3,10 +3,17 @@
 # import gage_config
 import config
 import ultrasound
-import ephem
-import requests
+# import ephem
+from requests import post
+import datetime
 # import power
 
-print "DepthAIN is " + config.DepthAIN
-print "The depth in cm is " + str(round(ultrasound.checkDepth(6), 2))
-print ""
+# print "DepthAIN is " + config.DepthAIN
+# print "The depth in cm is " + str(round(ultrasound.checkDepth(6), 2))
+# print ""
+
+level = ultrasound.checkDepth()
+timestamp = datetime.datetime.now()
+battery = 4.0
+payload = {'level': level, 'battery': battery, 'timestamp': timestamp}
+sample = post(config.PostURL, data=payload)
