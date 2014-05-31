a531c7e1962315c73876437c55f9885fe7b94a5b
Alex Kerney <abk@mac.com>
2014-05-31 07:40:29 -0400

    Lets try an updated gage.py, it’s probably going to explode everywhere

--- gage.py
+++ gage.py
@@ -26,38 +26,71 @@ class Sample(BaseModel):
 	volts = FloatField()
 	amps = FloatField()
 	uploaded = BooleanField()
-	result = CharField()
-	serverID = IntegerField()
+	result = CharField(null=True)
+	server_sample_id = IntegerField(null=True)
 
 class Config(BaseModel):
 	timing = IntegerField()
 	url = CharField()
-	
-	
 
-
-
-def send_results(destination):
+def get_results():
 	level = ultrasound.checkDepth()
 	timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") # remove the microsecond http://stackoverflow.com/questions/7999935/python-datetime-to-string-without-microsecond-component
-	battery = power.checkPower()
-	payload = {'level': level, 'battery': battery, 'timestamp': timestamp}
-	try:
-		sample = post(config.PostURL, data=payload, auth=(config.Id, config.Password))
-	except Exception as detail:
-		with open('status.txt', 'ab') as the_file:
-			the_file.write(str(detail))
-	with open('status.txt', 'ab') as the_file:
-		the_file.write(str(payload))
-		the_file.write('\n')
-	print timestamp, battery, level
-	return True
+	volts = power.checkVolts()
+	amps = power.checkAmps()
+	new_sample = Sample.create(timestamp=timestamp, level=level, volts=volts, amps=amps, uploaded=False)
+
+
+
+def send_results(destination,id,password):
+	for sample in Sample.select().where(Sample.uploaded == False).order_by(Sample.timestamp.asc()):
+		payload = {'level': sample.level, 'battery': sample.volts, 'timestamp': sample.timestamp}
+		try:
+			submit_sample = post(destination, data=payload, auth=(id, password), timeout=10)
+			if submit_sample.status_code == 201:
+				sample.uploaded = true
+				sample.result = submit_sample.json()
+				with open('/boot/uboot/status.txt', 'ab') as status_file:
+					status_file.write('Sample uploaded at ')
+					status_file.write(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")))
+					status_file.write(' ')
+					status_file.write(submit_sample.json())
+					status_file.write(' ')
+		with Exception as detail:
+			sample.result = detail
+			sample.uploaded = False
+			with open('/boot/uboot/status.txt', 'ab') as status_file:
+				status_file.write('Failed upload at ')
+				status_file.write(str(detail))
+				status_file.write(' ')
+		with open('/boot/uboot/status.txt', 'ab') as status_file:
+			status_file.write(str(payload))
+			status_file.write('\n')
+		return True
+		
+
+
+#	level = ultrasound.checkDepth()
+#	timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") # remove the microsecond http://stackoverflow.com/questions/7999935/python-datetime-to-string-without-microsecond-component
+#	battery = power.checkPower()
+#	payload = {'level': level, 'battery': battery, 'timestamp': timestamp}
+#	try:
+#		sample = post(config.PostURL, data=payload, auth=(config.Id, config.Password))
+#	except Exception as detail:
+#		with open('\boot\uboot\status.txt', 'ab') as the_file:
+#			the_file.write(str(detail))
+#	with open('\boot\uboot\status.txt', 'ab') as the_file:
+#		the_file.write(str(payload))
+#		the_file.write('\n')
+#	print timestamp, battery, level
+#	return True
 
 
 if __name__ == '__main__':
 	if os.path.isfile("/boot/uboot/gagerun") and not os.path.isfile("/boot/uboot/gagestop"):
 		print 'This program is running as __main__.'		
 		while True:
+			get_results()
 			time.sleep(60)
 			send_results(config.PostURL)
 			if not os.path.isfile("/boot/uboot/gagestop"):
