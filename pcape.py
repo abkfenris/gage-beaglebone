a77dc0b628281b02051f46bb75f49575eb04372f
Alex Kerney <abk@mac.com>
2014-08-25 08:21:27 -0400

    Testing out controlling the PowerCape from Python

---
+++ pcape.py
@@ -0,0 +1,13 @@
+import config
+from Adafruit_I2C import Adafruit_I2C
+
+cycleTime = config.CycleTime
+
+powercapeI2C = Adafruit_I2C(0x21)
+
+def setTime(cycleTime):
+	try:
+		i2c.write8(7, cycleTime)
+	except Exception, e:
+		with open('/boot/uboot/gage-status.txt', 'ab') as status_file:
+					status_file.write(str(e))
