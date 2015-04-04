a6af554f8faa07c5f62835dddfde78ab13c5b448
Alex Kerney <abk@mac.com>
2015-04-04 11:36:49 -0400

    Adding gage-client

--- gage.py
+++ gage.py
@@ -5,45 +5,112 @@ import config
 import ultrasound
 # import ephem
 from requests import post
-from requests.auth import HTTPBasicAuth
 import datetime
 import power
+import pcape
 import time
 import os.path
 import os
+from peewee import *
 
-# print "DepthAIN is " + config.DepthAIN
-# print "The depth in cm is " + str(round(ultrasound.checkDepth(6), 2))
-# print ""
+from gage_client.gage_client.client import Client, SendError
+
+db = SqliteDatabase('/boot/uboot/gage.db')
 
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-def send_results(destination):
-	level = ultrasound.checkDepth()
-	timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") # remove the microsecond http://stackoverflow.com/questions/7999935/python-datetime-to-string-without-microsecond-component
-	battery = power.checkPower()
-	payload = {'level': level, 'battery': battery, 'timestamp': timestamp}
-	try:
-		sample = post(config.PostURL, data=payload, auth=(config.Id, config.Password))
-	except Exception as detail:
-		with open('status.txt', 'ab') as the_file:
-			the_file.write(str(detail))
-	with open('status.txt', 'ab') as the_file:
-		the_file.write(str(payload))
-		the_file.write('\n')
-	print timestamp, battery, level
-	return True
+
+class Sample(BaseModel):
+    timestamp = DateTimeField()
+    level = FloatField()
+    volts = FloatField()
+    amps = FloatField()
+    uploaded = BooleanField()
+    result = TextField(null=True)
+
+
+class Config(BaseModel):
+    timing = IntegerField()
+    url = CharField()
+
+
+def get_sample():
+    level = ultrasound.checkDepth()
+    # remove the microsecond
+    # http://stackoverflow.com/questions/7999935/python-datetime-to-string-without-microsecond-component
+    timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    volts = power.checkVolts()
+    amps = power.checkAmps()
+    new_sample = Sample.create(timestamp=timestamp,
+                               level=level,
+                               volts=volts,
+                               amps=amps,
+                               uploaded=False)
+
+
+def send_samples(destination=config.PostURL,
+                 id=config.Id,
+                 password=config.Password):
+
+    # set up client
+    client = Client(destination, id, password)
+
+    # get samples that need to be sent and add to readings queue
+    for sample in Sample.select().where(Sample.uploaded is False).order_by(
+            Sample.timestamp.asc()):
+        client.reading('level', str(sample.timestamp), sample.level)
+        client.reading('volts', str(sample.timestamp), sample.volts)
+        client.reading('amps', str(sample.timestamp), sample.amps)
+
+    # try to send and write result to status file
+    try:
+        result, sucessful_ids = client.send_all()
+        with open('/boot/uboot/gage-status.txt', 'ab') as status_file:
+            status_file.write('Successfully sent at {dt} to {url}'.format(
+                dt=datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+                url=destination,
+            ))
+            status_file.write('  sucess fully uploaded: {sucess}'.format(
+                success=sucessful_ids
+            ))
+    except SendError as e:
+        sucessful_ids = e.sucessful
+        with open('/boot/uboot/gage-status.txt', 'ab') as status_file:
+            status_file.write('Send error at {dt} to {url}, {detail}'.format(
+                dt=datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+                url=destination,
+                detail=e
+            ))
+            status_file.write('  failed to upload: {failed}'.format(
+                failed=e.fail
+            ))
+            status_file.write('  sucess fully uploaded: {sucess}'.format(
+                success=e.sucessful
+            ))
+
+    # for every id that was sent mark as uploaded
+    for sample_id in sucessful_ids:
+        sample = Sample.get(Sample.id == sample_id)
+        sample.uploaded = True
+        sample.save()
 
 
 if __name__ == '__main__':
-	if os.path.isfile("/boot/uboot/gagerun") and not os.path.isfile("/boot/uboot/gagestop"):
-		print 'This program is running as __main__.'		
-		while True:
-			time.sleep(60)
-			send_results(config.PostURL)
-			if not os.path.isfile("/boot/uboot/gagestop"):
-				os.system("shutdown -h now")
-	else:
-		print 'gagestop is in /boot/uboot/ or gagerun is not.'
+    if os.path.isfile("/boot/uboot/gagerun") and not os.path.isfile("/boot/uboot/gagestop"):
+        print 'This program is running as __main__.'
+        while True:
+            time.sleep(45)
+            get_sample()
+            pcape.setTime(config.CycleTime)
+            time.sleep(15)
+            send_samples()
+            if not os.path.isfile("/boot/uboot/gagestop"):
+                os.system("shutdown -h now")
+    else:
+        print 'gagestop is in /boot/uboot/ or gagerun is not.'
+        exit()
 else:
-	print 'gage.py is imported'
+    print 'gage.py is imported'
