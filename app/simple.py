197f9416a49d39ea7a0912b87b851bc6e7f96d44
Alex Kerney <abk@mac.com>
2017-04-09 20:43:34 -0400

    Restarts Docker container on env change anyways

--- app/simple.py
+++ app/simple.py
@@ -3,6 +3,8 @@ import serial
 
 PORT = '/dev/ttyS2'
 
+wait = int(os.environ.get('GAGE_SIMPLE_WAIT', 5))
+
 ser = serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1)
 
 def read_serial():
@@ -16,4 +18,4 @@ def read_serial():
 while True:
     data = read_serial()
     print(datetime.datetime.now(), data)
-    time.sleep(int(os.environ.get('GAGE_SIMPLE_WAIT', 5)))
+    time.sleep(wait)
