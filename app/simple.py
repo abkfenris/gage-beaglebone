2e3f4ce08e4db68f012c64ddcb0cb0f4c6eec341
Alex Kerney <abk@mac.com>
2017-04-21 07:26:59 -0400

    Minimum voltage check and log formatter changes

--- app/simple.py
+++ app/simple.py
@@ -13,7 +13,7 @@ DATA_CSV_FOLDER = os.environ.get('GAGE_DATA_CSV_FOLDER', STORAGE_MOUNT_PATH + '/
 FILE_LOG_FOLDER = os.environ.get('GAGE_FILE_LOG_FOLDER', STORAGE_MOUNT_PATH + '/syslogs/')
 MAX_LOG_FILES = int(os.environ.get('MAX_LOG_FILES', 3))
 WAIT = int(os.environ.get('GAGE_SAMPLE_WAIT', 5))
-MIN_VOLTAGE = float(os.environ.get('GAGE_MIN_VOLTAGE', 3.2))
+MIN_VOLTAGE = float(os.environ.get('GAGE_MIN_VOLTAGE', 3.0))
 CELL_TYPE = os.environ.get('GAGE_CELL_TYPE', 'ting-sierra-250u')
 SAMPLES_PER_RUN = int(os.environ.get('GAGE_SAMPLES_PER_RUN', 10))
 PRE_SHUTDOWN_TIME = int(os.environ.get('GAGE_PRE_SHUTDOWN_TIME', 30))
@@ -28,12 +28,14 @@ MAX_STD_DEV = int(os.environ.get('GAGE_MAX_STD_DEV', 100))
 
 # Power control Environment Variables
 POWER_CONSERVE = bool(os.environ.get('GAGE_POWER_CONSERVE', False))
-RESTART_TIME = int(os.environ.get('GAGE_RESTART_TIME', 14))
+RESTART_TIME = int(os.environ.get('GAGE_RESTART_TIME', 10))
+MIN_VOLTAGE_RESTART_TIME = int(os.environ.get('GAGE_MIN_VOLTAGE_RESTART_TIME', 25))
 WATCHDOG_RESET_TIMEOUT = int(os.environ.get('GAGE_WATCHDOG_RESET_TIMEOUT', 300))
 WATCHDOG_POWER_TIMEOUT = int(os.environ.get('GAGE_WATCHDOG_POWER_TIMEOUT', 300))
 WATCHDOG_STOP_POWER_TIMEOUT = int(os.environ.get('GAGE_WATCHDOG_STOP_POWER_TIMEOUT', 300))
 WATCHDOG_START_POWER_TIMEOUT = int(os.environ.get('GAGE_WATCHDOG_START_POWER_TIMEOUT', 60))
 STARTUP_REASONS = os.environ.get('GAGE_STARTUP_REASONS', '0x09')
+
 
 # Logging levels
 STDOUT_LOG_LEVEL = os.environ.get('GAGE_STDOUT_LOG_LEVEL', 'WARNING').upper()
@@ -49,7 +51,7 @@ log_levels = {'DEBUG': logging.DEBUG,
 logger = logging.getLogger('gage')
 logger.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(log_levels.get(STDOUT_LOG_LEVEL, logging.WARNING))
@@ -150,7 +152,7 @@ def sensor_cycle(ser):
         else:
             flow = 'discharging'
 
-        logger.info(f'{date} {distance}mm {volts}v {flow} {amps}ma')
+        logger.info(f'{distance}mm {volts}v {flow} {amps}ma')
         writerow((date, distance, 'mm ultrasound', volts, flow, amps))
 
         time.sleep(WAIT)
@@ -209,9 +211,6 @@ if __name__ == '__main__':
     leds = pcape.StatusLEDs()
     leds.led_1, leds.led_2 = False, False
 
-    # setup cell
-    sprint.Sierra250U()
-    
     # mount SD and clean out old log files
     SD_CARD = sd_avaliable()
     
@@ -251,7 +250,19 @@ if __name__ == '__main__':
     else:
         logger.error('Micro SD card not avaliable for file storage')
         STOP = False
+    
+    if power.checkVolts() < MIN_VOLTAGE:
+        logger.error(f'System voltage has fallen below minimum voltage specified ({MIN_VOLTAGE} V). Shutting down for {MIN_VOLTAGE_RESTART_TIME} min to charge.')
+        
+        pcape.set_startup_reasons(STARTUP_REASONS)
+        pcape.set_cape_time()
+        pcape.set_time(MIN_VOLTAGE_RESTART_TIME)
 
+        supervisor.shutdown()
+
+    # setup cell
+    sprint.Sierra250U()
+    
     # setup serial
     ser = serial_setup()
 
