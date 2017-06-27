3fd1da655fabfa96784be19b5d3fa84ecfb58ca9
Alex Kerney <abk@mac.com>
2017-06-27 19:03:51 -0400

    Still need logging for filehandler

---
+++ app/exceptions.py
@@ -0,0 +1,12 @@
+"""
+Exceptions that the gage may encounter
+"""
+
+class SensorError(Exception):
+    pass
+
+class SamplingError(Exception):
+    pass
+
+class TooFewSamples(SamplingError):
+    pass
--- app/ultrasound.py
+++ app/ultrasound.py
@@ -6,7 +6,7 @@ import logging, subprocess
 
 import serial
 
-import config
+import config, exceptions
 
 logger = logging.getLogger('gage')
 
@@ -24,3 +24,16 @@ def serial_setup():
         logger.info(f'{config.UART} added to capemgr')
     ser = serial.Serial(port=config.PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1)
     return ser
+
+
+def read_serial(ser):
+    """Make 50 attempts to read the sensor serial data. Returns the first valid response"""
+    for _ in range(50):
+        ser.flushInput()
+        data = ser.read(5).decode('ASCII')
+        if len(data) == 5:
+            if data[0] == 'R' and data[1:5].isdigit(): # and data[1:5] != '9999'
+                return int(data[1:5])
+        logger.debug('Serial returned invalid info, trying again')
+    logger.error('Serial did not return valid info in 50 tries')
+    raise exceptions.SensorError
